f63b404568d419e6ed17805847a0c8acae27d915
ashishjaiswar21 <ashishkumar24569@gmail.com>
2026-02-17 03:39:43 +0530

    Final production deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,15 @@ import requests
 
 st.set_page_config(page_title="Car Price Prediction", page_icon="🚗", layout="centered")
 
+# API_URL = (
+#     "https://car-reselling-price-prediction.onrender.com/predict"
+#     or "http://127.0.0.1:8000/predict"
+# )  # change if your endpoint differs
 API_URL = (
-    "https://car-prediction-lpfl.onrender.com/predict"
-    or "http://127.0.0.1:8000/predict"
+    "https://car-reselling-price-prediction.onrender.com/predict" or 
+    "http://127.0.0.1:8000/predict"
 )  # change if your endpoint differs
+
 
 st.title("🚗 Car Price Prediction")
 st.caption(
